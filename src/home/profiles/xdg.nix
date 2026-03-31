64a1657bd374ec65226c9c9db0e44d94da20ba89
Bingshan Chang <chang@bingshan.org>
2026-03-31 08:59:34 +0000

    home: xdg: Enable XDG defaults
    * .dir-locals.el: Add xdg to Nix keyword list.
    * src/home/.gitkeep: Remove placeholder file from src/home.
    * src/home/profiles/xdg.nix: Add XDG defaults and user directories
    under data directory.

---
+++ src/home/profiles/xdg.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (config.xdg)
+    dataHome
+    ;
+
+  inherit (lib)
+    mkDefault
+    ;
+in
+{
+  home = {
+    preferXdgDirectories = mkDefault true;
+  };
+
+  xdg = {
+    enable = mkDefault true;
+
+    mimeApps = {
+      enable = mkDefault true;
+    };
+
+    userDirs = {
+      enable = mkDefault true;
+      createDirectories = mkDefault true;
+      desktop = "${dataHome}/Desktop";
+      documents = "${dataHome}/Documents";
+      download = "${dataHome}/Downloads";
+      music = "${dataHome}/Music";
+      pictures = "${dataHome}/Pictures";
+      publicShare = "${dataHome}/Public";
+      templates = "${dataHome}/Templates";
+      videos = "${dataHome}/Videos";
+    };
+  };
+}
