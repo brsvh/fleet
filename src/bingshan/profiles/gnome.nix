f42918c78c9c0972314d7ab1ff9f7df09a50f512
Bingshan Chang <chang@bingshan.org>
2026-04-07 03:00:57 +0000

    bingshan: gnome: Add XDG monitors config
    * src/bingshan/profiles/gnome.nix: Add XDG monitors config from
    azaleoid.

--- src/bingshan/profiles/gnome.nix
+++ src/bingshan/profiles/gnome.nix
@@ -1,16 +1,26 @@
 {
+  azaleoid,
   home,
   ...
 }:
 {
   imports = [
     home.profiles.gnome
+    home.profiles.xdg
   ];
 
   dconf = {
     settings = {
       "org/gnome/desktop/session" = {
         idle-delay = 0;
+      };
+    };
+  };
+
+  xdg = {
+    configFile = {
+      "monitors.xml" = {
+        source = azaleoid.etc.monitors;
       };
     };
   };
