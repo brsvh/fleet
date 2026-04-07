5e307e6f0ccb0dcb343d360be78a290ae23f2a9a
Bingshan Chang <chang@bingshan.org>
2026-04-07 02:42:45 +0000

    bingshan: gnupg: Update gpg-agent SSH key
    * src/bingshan/profiles/gnupg.nix: Update gpg-agent SSH key.

--- src/bingshan/profiles/gnupg.nix
+++ src/bingshan/profiles/gnupg.nix
@@ -10,7 +10,7 @@
   services = {
     gpg-agent = {
       sshKeys = [
-        "F0270F0B2CDA621249E7A6B5A37817FE5B501BF1"
+        "54A37140E8C5AD1C6B17C257AE6358E4C0B4ECE6"
       ];
     };
   };
