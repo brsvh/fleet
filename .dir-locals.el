fecb6df92dd8519445907af5dd05387f109a96fa
Bingshan Chang <chang@bingshan.org>
2026-04-07 02:55:35 +0000

    azaleoid: monitors: Add monitor layout configuration
    * .dir-locals.el: Add layoutmode, logicalmonitor, and monitorspec
    words to the dictionary.
    * src/azaleoid/etc/monitors.xml: Add a logical monitor layout for the
    eDP-1 panel.

--- .dir-locals.el
+++ .dir-locals.el
@@ -24,17 +24,18 @@ TriggerKeys Zenkaku adwaita allowUnfree apm autocd azaleoid bg bgrt
 bingshan bluetooth btrfs cachix cdspell cgroups checkwinsize cmdhist
 cond config consolefonts ctrl dae dconf dev devShells devshell
 devshells dialout dirToAttrs direnv dirname dirspell disko
-editorconfig efi emacs enablement erasedups esac eurlatgr facter fg
-filesystem gdm gitattributes gitignore globstar gnome gnupg gpg gpt
+editorconfig edP efi emacs enablement erasedups esac eurlatgr facter
+fg filesystem gdm gitattributes gitignore globstar gnome gnupg gpg gpt
 gtk gtkrc guix gz histappend histfile hstr hstrnotiocsti ignoreboth
-ignorespace infix jackaudio json kvm lanzaboote lba lefthook lf
-libvirtd linux loglevel loopback lp mkDefault mkdir networkmanager
-nftables nixfmt nixos nixosConfigurations nixpkgs noatime noclobber
-openssh partlabel pinentry pipefail plymouth pnp posix psfu rebase
-resetStateWhenFocusIn shopt showInputMethodInformationWhenFocusIn
-smbios src ssd stdout subvols swapfile symlinked symlinks sysfs
-systemd toml treefmt tty txt udev uefi uids untracked usb utf vbe vfat
-virtualisation wayland writeback xdg xwayland yaml yml zh zram zstd")))
+ignorespace infix jackaudio json kvm lanzaboote layoutmode lba
+lefthook lf libvirtd linux logicalmonitor loglevel loopback lp
+mkDefault mkdir monitorspec networkmanager nftables nixfmt nixos
+nixosConfigurations nixpkgs noatime noclobber openssh partlabel
+pinentry pipefail plymouth pnp posix psfu rebase resetStateWhenFocusIn
+shopt showInputMethodInformationWhenFocusIn smbios src ssd stdout
+subvols swapfile symlinked symlinks sysfs systemd toml treefmt tty txt
+udev uefi uids untracked usb utf vbe vfat virtualisation wayland
+writeback xdg xml xwayland yaml yml zh zram zstd")))
 
  (nix-mode
   .
