52f587455400be54d666b2ec7cdbf01148d090d7
Bingshan Chang <chang@bingshan.org>
2026-04-10 09:00:30 +0000

    system: fonts: Add fontconfig defaults
    * src/system/profiles/fonts.nix: Add a shared fontconfig profile with
    sensible rendering and compatibility defaults.

---
+++ src/system/profiles/fonts.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkDefault
+    ;
+in
+{
+  fonts = {
+    fontconfig = {
+      allowBitmaps = mkDefault false;
+      allowType1 = mkDefault false;
+      cache32Bit = mkDefault false;
+
+      enable = mkDefault true;
+
+      hinting = {
+        autohint = mkDefault false;
+        enable = mkDefault true;
+        style = "slight";
+      };
+
+      includeUserConf = mkDefault true;
+
+      subpixel = {
+        rgba = mkDefault "none";
+      };
+
+      useEmbeddedBitmaps = mkDefault false;
+    };
+  };
+}
