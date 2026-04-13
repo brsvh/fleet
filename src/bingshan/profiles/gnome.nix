ab74ca7ff5e7c705bc9f34bd553d959f9f12ae4c
Bingshan Chang <chang@bingshan.org>
2026-04-13 01:50:43 +0000

    bingshan: gnome: Configure desktop fonts and power settings
    * src/bingshan/profiles/gnome.nix: Import font and GTK profiles, set
    GNOME interface fonts, and disable idle dimming and AC sleep.

--- src/bingshan/profiles/gnome.nix
+++ src/bingshan/profiles/gnome.nix
@@ -1,18 +1,44 @@
 {
   azaleoid,
+  bingshan,
+  config,
   home,
+  lib,
   ...
 }:
+let
+  inherit (lib)
+    elemAt
+    toString
+    ;
+
+  first = list: elemAt list 0;
+
+  fonts = config.fonts.fontconfig.defaultFonts;
+in
 {
   imports = [
+    bingshan.profiles.fonts
+    bingshan.profiles.gtk
     home.profiles.gnome
     home.profiles.xdg
   ];
 
   dconf = {
     settings = {
+      "org/gnome/desktop/interface" = {
+        font-name = "${first fonts.sansSerif} ${toString config.gtk.font.size}";
+        document-font-name = "${first fonts.serif} ${toString config.gtk.font.size}";
+        monospace-font-name = "${first fonts.monospace} ${toString config.gtk.font.size}";
+      };
+
       "org/gnome/desktop/session" = {
         idle-delay = 0;
+      };
+
+      "org/gnome/settings-daemon/plugins/power" = {
+        idle-dim = false;
+        sleep-inactive-ac-type = "nothing";
       };
     };
   };
