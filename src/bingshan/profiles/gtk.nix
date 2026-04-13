fccdf2d4c027aae520d6e62683f26bfd4639b845
Bingshan Chang <chang@bingshan.org>
2026-04-13 01:43:23 +0000

    bingshan: gtk: Set gtk font to be same as fonts profile
    * src/bingshan/profiles/gtk.nix: Set the GTK font and package to be
    the same as the fonts profile.

---
+++ src/bingshan/profiles/gtk.nix
@@ -0,0 +1,32 @@
+{
+  bingshan,
+  config,
+  home,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    elemAt
+    mkForce
+    toString
+    ;
+
+  first = list: elemAt list 0;
+
+  fonts = config.fonts.fontconfig.defaultFonts;
+in
+{
+  imports = [
+    bingshan.profiles.fonts
+    home.profiles.gtk
+  ];
+
+  gtk = {
+    font = {
+      name = mkForce "${first fonts.sansSerif}";
+      package = mkForce pkgs.windowns-fonts;
+    };
+  };
+}
