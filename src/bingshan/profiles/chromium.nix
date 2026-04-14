e604a13eb0fa95e8bb3132169cef64eb4cfe415b
Bingshan Chang <chang@bingshan.org>
2026-04-14 07:04:42 +0000

    bingshan: chromium: Set default web MIME applications
    * .dir-locals.el: Add web MIME type spellings to local dictionary.
    * src/bingshan/profiles/chromium.nix: Register Chromium as the default
    handler for web MIME types and schemes.

--- src/bingshan/profiles/chromium.nix
+++ src/bingshan/profiles/chromium.nix
@@ -1,7 +1,24 @@
 {
+  config,
   home,
   ...
 }:
+let
+  chromiumDesktop = "google-chrome.desktop";
+
+  chromiumAssociation = {
+    "application/atom+xml" = [ chromiumDesktop ];
+    "application/pdf" = [ chromiumDesktop ];
+    "application/rss+xml" = [ chromiumDesktop ];
+    "application/xhtml+xml" = [ chromiumDesktop ];
+    "application/xml" = [ chromiumDesktop ];
+    "image/svg+xml" = [ chromiumDesktop ];
+    "text/html" = [ chromiumDesktop ];
+    "text/xml" = [ chromiumDesktop ];
+    "x-scheme-handler/http" = [ chromiumDesktop ];
+    "x-scheme-handler/https" = [ chromiumDesktop ];
+  };
+in
 {
   imports = [
     home.profiles.chromium
@@ -12,6 +29,16 @@
       commandLineArgs = [
         "--enable-features=VerticalTabs"
       ];
+    };
+  };
+
+  xdg = {
+    mimeApps = {
+      defaultApplicationPackages = [
+        config.programs.chromium.package
+      ];
+
+      defaultApplications = chromiumAssociation;
     };
   };
 }
