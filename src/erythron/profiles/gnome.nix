f8b267ecd902df5a7dae994760d7edb4b503d64e
Bingshan Chang <chang@bingshan.org>
2026-04-17 10:41:23 +0000

    erythron: gnome: Fix permission issue of monitors link
    * src/erythron/profiles/gnome.nix: Fix permission issue of monitors
    link.

--- src/erythron/profiles/gnome.nix
+++ src/erythron/profiles/gnome.nix
@@ -7,6 +7,9 @@
 }:
 let
   inherit (lib)
+    concatLists
+    concatStringsSep
+    escapeShellArg
     filterAttrs
     mapAttrsToList
     ;
@@ -14,6 +17,16 @@ let
   normalUsers = filterAttrs (
     _: v: v.enable && v.isNormalUser
   ) config.users.users;
+
+  userGroup =
+    user:
+    let
+      group = config.users.users.${user}.group;
+    in
+    if group == null || group == "" then
+      "users"
+    else
+      group;
 in
 {
   imports = [
@@ -22,10 +35,39 @@ in
 
   systemd = {
     tmpfiles = {
-      rules = mapAttrsToList (
-        _: v:
-        "L+ ${v.home}/.config/monitors.xml - - - - ${erythron.etc.monitors}"
-      ) normalUsers;
+      rules = concatLists (
+        mapAttrsToList (n: v: [
+          "d ${v.home}/.config 0700 ${v.name} ${userGroup n} -"
+          "L+ ${v.home}/.config/monitors.xml - - - - ${erythron.etc.monitors}"
+        ]) normalUsers
+      );
     };
   };
+
+  system = {
+    activationScripts = {
+      userMonitorsPermission = {
+        text = concatStringsSep "\n" (
+          mapAttrsToList (n: v: ''
+            cfg=${v.home}/.config
+
+            if [ -L "$cfg" ]; then
+              :
+            elif [ -d "$cfg" ]; then
+              chown ${v.name}:${userGroup n} "$cfg"
+              chmod 0700 "$cfg"
+            else
+              install -d -m 0700 -o ${v.name} -g ${userGroup n} "$cfg"
+            fi
+
+            rm -rf "$cfg/monitors.xml"
+            ln -s ${erythron.etc.monitors} "$cfg/monitors.xml"
+
+            chown -h ${v.name}:${userGroup n} "$cfg/monitors.xml"
+          '') normalUsers
+        );
+      };
+    };
+  };
+
 }
