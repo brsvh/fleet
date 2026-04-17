42c5a3e0c6ad00c9ad696ed145c59cff2259d151
Bingshan Chang <chang@bingshan.org>
2026-04-17 06:52:05 +0000

    erythron: gnome: Link monitor configuration
    * src/erythron/profiles/gnome.nix: Add tmpfiles rules that link
    normal users' monitor configuration to the shared Erythron file.

--- src/erythron/profiles/gnome.nix
+++ src/erythron/profiles/gnome.nix
@@ -1,9 +1,31 @@
 {
+  erythron,
+  config,
+  lib,
   system,
   ...
 }:
+let
+  inherit (lib)
+    filterAttrs
+    mapAttrsToList
+    ;
+
+  normalUsers = filterAttrs (
+    _: v: v.enable && v.isNormalUser
+  ) config.users.users;
+in
 {
   imports = [
     system.profiles.gnome
   ];
+
+  systemd = {
+    tmpfiles = {
+      rules = mapAttrsToList (
+        _: v:
+        "L+ ${v.home}/.config/monitors.xml - - - - ${erythron.etc.monitors}"
+      ) normalUsers;
+    };
+  };
 }
