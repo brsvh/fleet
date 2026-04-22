25031a34431f3463837ed6b7f765c4a6e1181727
Bingshan Chang <chang@bingshan.org>
2026-04-22 03:25:21 +0000

    system: docker: Update docker group membership
    * src/system/profiles/docker.nix: Add normal users to the
    docker group while keeping the existing Docker profile defaults.

--- src/system/profiles/docker.nix
+++ src/system/profiles/docker.nix
@@ -5,8 +5,18 @@
 }:
 let
   inherit (lib)
+    filterAttrs
+    mapAttrsToList
     mkDefault
     ;
+
+  normalUsers = filterAttrs (
+    _: v: v.enable && v.isNormalUser
+  ) config.users.users;
+
+  normalUsersList = mapAttrsToList (
+    _: value: value.name
+  ) normalUsers;
 
   storageDriver =
     if config.fileSystems."/".fsType == "btrfs" then
@@ -15,6 +25,14 @@ let
       null;
 in
 {
+  users = {
+    groups = {
+      docker = {
+        members = normalUsersList;
+      };
+    };
+  };
+
   virtualisation = {
     docker = {
       inherit
