567ac4351f28e4ca72154aabc667cfa80b5e9549
Bingshan Chang <chang@bingshan.org>
2026-04-22 04:02:24 +0000

    system: network-manager: Update group membership
    * src/system/profiles/network-manager.nix: Add all normal
    users to the network-manager group from the shared profile.

--- src/system/profiles/network-manager.nix
+++ src/system/profiles/network-manager.nix
@@ -1,16 +1,35 @@
 {
+  config,
   lib,
   ...
 }:
 let
   inherit (lib)
+    filterAttrs
+    mapAttrsToList
     mkDefault
     ;
+
+  normalUsers = filterAttrs (
+    _: v: v.enable && v.isNormalUser
+  ) config.users.users;
+
+  normalUsersList = mapAttrsToList (
+    _: value: value.name
+  ) normalUsers;
 in
 {
   networking = {
     networkmanager = {
       enable = mkDefault true;
+    };
+  };
+
+  users = {
+    groups = {
+      network-manager = {
+        members = normalUsersList;
+      };
     };
   };
 }
