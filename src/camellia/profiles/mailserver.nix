ac4c8e0a97910bc59f3d892575926ae7b6fcce37
Bingshan Chang <chang@bingshan.org>
2026-05-18 10:06:11 +0000

    camellia: mailserver: Add mailserver profile
    * src/camellia/profiles/mailserver.nix: Add a host mailserver
    profile with the bingshan.org account and networking defaults.

---
+++ src/camellia/profiles/mailserver.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  system,
+  ...
+}:
+{
+  imports = [
+    system.profiles.mailserver
+    system.profiles.networking
+  ];
+
+  mailserver = {
+    accounts = {
+      "chang@bingshan.org" = {
+        hashedPassword = "$y$j9T$f8cxmklj9ft1Sq6iZnMcM1$DnX/HiX8v384G5eUVCYChHxk44Z0348WP/s.btjRUH.";
+      };
+    };
+
+    domains = [
+      config.networking.domain
+    ];
+
+    fqdn = config.networking.fqdn;
+  };
+}
