a9f5dcf09cc73635807d8d1873ec92c4a5bbcd18
Bingshan Chang <chang@bingshan.org>
2026-05-18 12:06:23 +0000

    bingshan: accounts: Configure mail account
    * src/bingshan/etc/password-store/mail.bingshan.org/
    chang@bingshan.org.gpg: Add encrypted mail account password entry.
    * src/bingshan/profiles/accounts.nix: Import shared mail account
    profile and enable IMAP, SMTP, msmtp, mu, and OfflineIMAP.

--- src/bingshan/profiles/accounts.nix
+++ src/bingshan/profiles/accounts.nix
@@ -1,13 +1,21 @@
 {
   config,
+  home,
   ...
 }:
 let
   inherit (config.home)
     username
     ;
+
+  email =
+    config.accounts.email.accounts.${username};
 in
 {
+  imports = [
+    home.profiles.accounts
+  ];
+
   accounts = {
     email = {
       accounts = {
@@ -19,6 +27,39 @@ in
             signByDefault = true;
           };
 
+          imap = {
+            host = "mail.bingshan.org";
+            port = 993;
+
+            tls = {
+              enable = true;
+            };
+          };
+
+          msmtp = {
+            enable = true;
+          };
+
+          mu = {
+            enable = true;
+          };
+
+          offlineimap = {
+            enable = true;
+
+            extraConfig = {
+              account = {
+                autorefresh = 20;
+              };
+
+              local = {
+                sync_deletes = true;
+              };
+            };
+          };
+
+          passwordCommand = "pass show ${imap.host}/${userName}";
+
           primary = true;
           realName = "Bingshan Chang";
 
@@ -29,6 +70,15 @@ in
               Pronoun: He/Him/His
               GPG: D6E9 ED45 04C4 1AD2 DA16  F396 31E6 2A2F C338 02BA
             '';
+          };
+
+          smtp = {
+            host = "mail.bingshan.org";
+            port = 465;
+
+            tls = {
+              enable = true;
+            };
           };
 
           userName = address;
