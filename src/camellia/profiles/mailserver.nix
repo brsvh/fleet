5268ff78f414343f110fc07193111bab72575333
Bingshan Chang <chang@bingshan.org>
2026-05-19 02:59:57 +0000

    camellia: mailserver: Add brsvh.org aliases
    * src/camellia/profiles/mailserver.nix: Add brsvh.org mail aliases for
    chang@bingshan.org and include brsvh.org in served domains.

--- src/camellia/profiles/mailserver.nix
+++ src/camellia/profiles/mailserver.nix
@@ -13,12 +13,23 @@
   mailserver = {
     accounts = {
       "chang@bingshan.org" = {
+        aliases = [
+          "abuse@brsvh.org"
+          "bot@brsvh.org"
+          "bsc@brsvh.org"
+          "open@brsvh.org"
+          "postmaster@brsvh.org"
+          "register@brsvh.org"
+          "steam@brsvh.org"
+        ];
+
         hashedPassword = "$y$j9T$f8cxmklj9ft1Sq6iZnMcM1$DnX/HiX8v384G5eUVCYChHxk44Z0348WP/s.btjRUH.";
       };
     };
 
     domains = [
       config.networking.domain
+      "brsvh.org"
     ];
 
     fqdn = "mail.bingshan.org";
