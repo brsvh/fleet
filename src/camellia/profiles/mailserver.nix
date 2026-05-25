33032898b272c15f08fbd1419d2316899ccd0dcf
Bingshan Chang <chang@bingshan.org>
2026-05-25 09:52:40 +0800

    camellia: mailserver: Add MTA-STS virtual hosts
    * src/camellia/profiles/mailserver.nix: Add MTA-STS virtual hosts.

--- src/camellia/profiles/mailserver.nix
+++ src/camellia/profiles/mailserver.nix
@@ -1,9 +1,48 @@
 {
   camellia,
   config,
+  lib,
+  pkgs,
   system,
   ...
 }:
+let
+  inherit (lib)
+    genAttrs
+    ;
+
+  inherit (pkgs)
+    writeTextDir
+    ;
+
+  cfg = config.mailserver;
+
+  mtaSts = {
+    host = {
+      enableACME = config.security.acme.acceptTerms;
+      forceSSL = config.security.acme.acceptTerms;
+
+      root = writeTextDir ".well-known/mta-sts.txt" ''
+        version: STSv1
+        mode: testing
+        mx: ${cfg.fqdn}
+        max_age: 604800
+      '';
+
+      locations = {
+        "= /.well-known/mta-sts.txt" = {
+          extraConfig = ''
+            default_type text/plain;
+          '';
+        };
+      };
+    };
+  };
+
+  mtaStsVirtualHosts = genAttrs (map (
+    domain: "mta-sts.${domain}"
+  ) cfg.domains) (_: mtaSts.host);
+in
 {
   imports = [
     camellia.profiles.sops
@@ -42,6 +81,12 @@
     ];
 
     fqdn = "mail.bingshan.org";
+  };
+
+  services = {
+    nginx = {
+      virtualHosts = mtaStsVirtualHosts;
+    };
   };
 
   sops = {
