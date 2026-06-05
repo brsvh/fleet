281b8f4cc286b3a846d51df5662db3637ef9f1b8
Bingshan Chang <chang@bingshan.org>
2026-06-05 09:31:49 +0800

    azaleoid: gdm: Use global monitor config
    * src/azaleoid/profiles/gdm.nix: Use /etc/xdg/monitors.xml for GDM.

--- src/azaleoid/profiles/gdm.nix
+++ src/azaleoid/profiles/gdm.nix
@@ -1,36 +1,18 @@
 {
-  config,
   azaleoid,
-  lib,
   system,
   ...
 }:
-let
-  inherit (lib)
-    concatLists
-    filterAttrs
-    hasPrefix
-    mapAttrsToList
-    ;
-
-  greeterUsers = filterAttrs (
-    name: user:
-    user.enable && hasPrefix "gdm-greeter" name
-  ) config.users.users;
-in
 {
   imports = [
     system.profiles.gdm
   ];
 
-  systemd = {
-    tmpfiles = {
-      rules = concatLists (
-        mapAttrsToList (name: user: [
-          "d ${user.home}/.config 0711 ${name} gdm"
-          "L+ ${user.home}/.config/monitors.xml - - - - ${azaleoid.etc.monitors}"
-        ]) greeterUsers
-      );
+  environment = {
+    etc = {
+      "xdg/monitors.xml" = {
+        source = azaleoid.etc.monitors;
+      };
     };
   };
 }
