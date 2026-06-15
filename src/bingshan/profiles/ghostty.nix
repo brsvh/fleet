acc0c6ec7d997d001698a00ade832177773846cc
Bingshan Chang <chang@bingshan.org>
2026-06-15 10:33:24 +0800

    bingshan: ghostty: Configure font and theme
    * src/bingshan/profiles/ghostty.nix: Import shared fonts and
    configure Ghostty font, size, and theme settings.

--- src/bingshan/profiles/ghostty.nix
+++ src/bingshan/profiles/ghostty.nix
@@ -1,9 +1,33 @@
 {
+  bingshan,
+  config,
   home,
+  lib,
   ...
 }:
+let
+  inherit (lib)
+    elemAt
+    toString
+    ;
+
+  first = list: elemAt list 0;
+
+  fonts = config.fonts.fontconfig.defaultFonts;
+in
 {
   imports = [
+    bingshan.profiles.fonts
     home.profiles.ghostty
   ];
+
+  programs = {
+    ghostty = {
+      settings = {
+        font-family = "${first fonts.monospace}";
+        font-size = config.gtk.font.size;
+        theme = "Modus Vivendi Tinted";
+      };
+    };
+  };
 }
