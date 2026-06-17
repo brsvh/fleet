34cbaa197badb8510966e04ad75dd7a5a80a26c7
Bingshan Chang <chang@bingshan.org>
2026-06-17 09:55:14 +0800

    bingshan: javascript: Use Node profile
    * src/bingshan/profiles/javascript.nix: Import node settings and set
    the Node REPL history size.

--- src/bingshan/profiles/javascript.nix
+++ src/bingshan/profiles/javascript.nix
@@ -5,7 +5,13 @@
 {
   imports = [
     home.profiles.bun
-    home.profiles.npm
+    home.profiles.node
     home.profiles.xdg
   ];
+
+  home = {
+    sessionVariables = {
+      NODE_REPL_HISTORY_SIZE = 10000;
+    };
+  };
 }
