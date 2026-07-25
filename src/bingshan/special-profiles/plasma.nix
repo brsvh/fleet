c6c225958484a3e5b1ae9db31e5a2911fb59c52d
Bingshan Chang <chang@bingshan.org>
2026-07-26 01:25:20 +0800

    bingshan: plasma: Configure panel layout
    * src/bingshan/special-profiles/plasma.nix: Configure the Plasma
    panel and its widgets.

--- src/bingshan/special-profiles/plasma.nix
+++ src/bingshan/special-profiles/plasma.nix
@@ -6,4 +6,70 @@
   imports = [
     home.profiles.plasma
   ];
+
+  programs = {
+    plasma = {
+      panels = [
+        {
+          alignment = "center";
+          floating = true;
+          height = 46;
+          hiding = "none";
+          lengthMode = "fill";
+          location = "top";
+          opacity = "adaptive";
+          screen = 0;
+
+          widgets = [
+            "org.kde.plasma.kickoff"
+            "org.kde.plasma.appmenu"
+            "org.kde.plasma.panelspacer"
+            {
+              iconTasks = {
+                launchers = [
+                  "preferred://browser"
+                ];
+              };
+            }
+            "org.kde.plasma.pager"
+            "org.kde.plasma.marginsseparator"
+            "org.kde.plasma.colorpicker"
+            {
+              systemTray = {
+                items = {
+                  extra = [
+                    "org.kde.plasma.cameraindicator"
+                    "org.kde.plasma.clipboard"
+                    "org.kde.plasma.devicenotifier"
+                    "org.kde.plasma.manage-inputmethod"
+                    "org.kde.plasma.mediacontroller"
+                    "org.kde.plasma.notifications"
+                    "org.kde.kscreen"
+                    "org.kde.plasma.battery"
+                    "org.kde.plasma.bluetooth"
+                    "org.kde.plasma.brightness"
+                    "org.kde.plasma.keyboardindicator"
+                    "org.kde.plasma.keyboardlayout"
+                    "org.kde.plasma.networkmanagement"
+                    "org.kde.plasma.volume"
+                    "org.kde.plasma.weather"
+                  ];
+                };
+              };
+            }
+            {
+              name = "org.kde.plasma.digitalclock";
+
+              config = {
+                Appearance = {
+                  fontWeight = 400;
+                };
+              };
+            }
+            "org.kde.plasma.showdesktop"
+          ];
+        }
+      ];
+    };
+  };
 }
