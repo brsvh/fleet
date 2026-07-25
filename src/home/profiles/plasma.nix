2018a428dcd3df3372b863df13306370bc3c2de2
Bingshan Chang <chang@bingshan.org>
2026-07-26 01:04:24 +0800

    home: plasma: Configure desktop defaults
    * src/home/profiles/plasma.nix: Configure Plasma desktop defaults
    and GTK, Qt, and Fcitx integration.

--- src/home/profiles/plasma.nix
+++ src/home/profiles/plasma.nix
@@ -1,8 +1,88 @@
 {
+  home,
+  inputs,
+  lib,
   pkgs,
   ...
 }:
+let
+  inherit (inputs)
+    plasma-manager
+    ;
+
+  inherit (lib)
+    mkDefault
+    ;
+in
 {
+  imports = [
+    home.profiles.gtk
+    home.profiles.qt
+    plasma-manager.homeModules.plasma-manager
+  ];
+
+  gtk = {
+    colorScheme = mkDefault "light";
+
+    cursorTheme = {
+      name = mkDefault "breeze_cursors";
+      package = mkDefault pkgs.kdePackages.breeze;
+    };
+
+    iconTheme = {
+      name = mkDefault "breeze";
+      package = mkDefault pkgs.kdePackages.breeze-icons;
+    };
+
+    theme = {
+      name = mkDefault "Breeze";
+      package = mkDefault pkgs.kdePackages.breeze-gtk;
+    };
+  };
+
+  i18n = {
+    inputMethod = {
+      fcitx5 = {
+        settings = {
+          addons = {
+            classicui = {
+              globalSection = {
+                "Theme" = mkDefault "plasma";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  programs = {
+    plasma = {
+      enable = mkDefault true;
+
+      workspace = {
+        colorScheme = mkDefault "BreezeLight";
+
+        cursor = {
+          theme = mkDefault "breeze_cursors";
+        };
+
+        iconTheme = mkDefault "breeze";
+        theme = mkDefault "default";
+      };
+    };
+  };
+
+  qt = {
+    platformTheme = {
+      name = mkDefault "kde";
+    };
+
+    style = {
+      name = mkDefault "breeze";
+    };
+  };
+
   services = {
     gpg-agent = {
       pinentry = {
