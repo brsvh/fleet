ebf0f7c0ca85730253506e9614c3e788bd4dbd52
Bingshan Chang <chang@bingshan.org>
2026-07-29 16:37:30 +0800

    bingshan: fonts: Set Emacs monospace faces
    * src/bingshan/profiles/fonts.nix: Configure the Emacs default and
    fixed-pitch faces from the preferred monospace family.

--- src/bingshan/profiles/fonts.nix
+++ src/bingshan/profiles/fonts.nix
@@ -9,11 +9,13 @@
 let
   inherit (builtins)
     readDir
+    toJSON
     ;
 
   inherit (lib)
     filterAttrs
     getExe'
+    head
     hasSuffix
     mapAttrs'
     mkForce
@@ -176,16 +178,22 @@ in
 
   programs = {
     emacs = {
-      extraConfig = ''
-        (use-package emacs
-          :demand t
-          :no-require t
-          :when (display-graphic-p)
+      extraConfig =
+        let
+          monospace = head config.fonts.fontconfig.defaultFonts.monospace;
+        in
+        ''
+          (use-package emacs
+            :demand t
+            :no-require t
+            :when (display-graphic-p)
 
-          :config
-          (set-fontset-font t 'cjk-misc (font-spec :family "Microsoft YaHei"))
-          (set-fontset-font t 'han (font-spec :family "Microsoft YaHei")))
-      '';
+            :config
+            (set-face-attribute 'default nil :family ${toJSON monospace})
+            (set-face-attribute 'fixed-pitch nil :family ${toJSON monospace})
+            (set-fontset-font t 'cjk-misc (font-spec :family "Microsoft YaHei"))
+            (set-fontset-font t 'han (font-spec :family "Microsoft YaHei")))
+        '';
     };
   };
 
