1e0c09e1117c436df69092f2b9e579ba735662cf
Bingshan Chang <chang@bingshan.org>
2026-08-11 06:26:32 +0800

    home: wemeet: Use XWayland desktop launcher
    * src/home/profiles/wemeet.nix: Launch Wemeet through XWayland.

--- src/home/profiles/wemeet.nix
+++ src/home/profiles/wemeet.nix
@@ -18,4 +18,27 @@ in
       enable = mkDefault true;
     };
   };
+
+  xdg = {
+    desktopEntries = {
+      wemeetapp = {
+        categories = [
+          "AudioVideo"
+        ];
+
+        exec = "wemeet-xwayland %u";
+        icon = "wemeet";
+
+        mimeType = [
+          "x-scheme-handler/wemeet"
+        ];
+
+        name = "WemeetApp";
+
+        settings = {
+          "Name[zh_CN]" = "腾讯会议";
+        };
+      };
+    };
+  };
 }
