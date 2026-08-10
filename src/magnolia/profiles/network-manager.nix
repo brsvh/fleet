9e1ce9e8accee693b5cf895c7e2f95d39d9fbf95
Bingshan Chang <chang@bingshan.org>
2026-08-10 13:03:59 +0800

    magnolia: network-manager: Configure TAP networking
    * src/magnolia/profiles/network-manager.nix: Add TAP profiles,
    forwarding rules, and a stable owner UID.

--- src/magnolia/profiles/network-manager.nix
+++ src/magnolia/profiles/network-manager.nix
@@ -2,8 +2,69 @@
   system,
   ...
 }:
+let
+  mkTapProfile =
+    {
+      gateway,
+      name,
+    }:
+    {
+      connection = {
+        autoconnect = true;
+        id = name;
+        interface-name = name;
+        type = "tun";
+      };
+
+      ipv4 = {
+        address1 = "${gateway}/24";
+        method = "shared";
+      };
+
+      ipv6 = {
+        method = "disabled";
+      };
+
+      tun = {
+        mode = 2;
+        owner = 1000;
+      };
+    };
+in
 {
   imports = [
     system.profiles.network-manager
   ];
+
+  networking = {
+    firewall = {
+      extraForwardRules = ''
+        iifname { "tap94", "tap95" } accept
+      '';
+    };
+
+    networkmanager = {
+      ensureProfiles = {
+        profiles = {
+          tap94 = mkTapProfile {
+            gateway = "192.168.94.1";
+            name = "tap94";
+          };
+
+          tap95 = mkTapProfile {
+            gateway = "192.168.95.1";
+            name = "tap95";
+          };
+        };
+      };
+    };
+  };
+
+  users = {
+    users = {
+      bingshan = {
+        uid = 1000;
+      };
+    };
+  };
 }
