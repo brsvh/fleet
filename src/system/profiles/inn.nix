3609cd3ce0090e5df22a74097a7ead893acec4af
Bingshan Chang <chang@bingshan.org>
2026-09-16 10:52:25 +0800

    system: inn: Move archive groups into the shared profile
    * src/system/profiles/inn.nix: Declare archive groups independently
    of the user news sources. Preserve group order and use the same lists
    for bootstrap, replication, and upstream synchronization.

--- src/system/profiles/inn.nix
+++ src/system/profiles/inn.nix
@@ -1,5 +1,4 @@
 {
-  bingshan,
   config,
   lib,
   system,
@@ -9,38 +8,96 @@ let
   inherit (lib)
     attrValues
     concatLists
-    filterAttrs
     mapAttrs
     mkDefault
     mkIf
     mkMerge
-    optionalAttrs
     pipe
     ;
 
-  news = filterAttrs (
-    _: source: source ? endpoint
-  ) (import bingshan.etc.news-sources);
+  # Keep bootstrap and replication group sets identical on every host.
+  archiveGroups = {
+    eternal-september = [
+      "comp.arch.fpga"
+      "alt.folklore.computers"
+      "alt.peeves"
+      "comp.emacs"
+      "rec.food.drink.tea"
+      "rec.games.video.classic"
+      "alt.comp.lang.rust"
+      "comp.lang.c"
+      "comp.lang.c++"
+      "comp.lang.haskell"
+      "comp.lang.lisp"
+      "comp.lang.scheme"
+      "comp.os.linux.networking"
+      "rec.music.makers.synth"
+      "rec.arts.books"
+      "comp.security.ssh"
+    ];
 
-  sources = mapAttrs (
-    _: source:
-    {
-      inherit (source)
-        endpoint
-        ;
+    gmane = [
+      "gmane.emacs.devel"
+      "gmane.emacs.help"
+      "gmane.lisp.asdf.devel"
+      "gmane.lisp.guile.devel"
+      "gmane.lisp.guile.user"
+      "gmane.lisp.scheme.chez"
+      "gmane.lisp.scheme.mit-scheme.devel"
+      "gmane.comp.kde.devel.general"
+      "gmane.comp.hardware.riscv.isa.devel"
+      "gmane.comp.hardware.riscv.opensbi.devel"
+      "gmane.linux.ports.riscv"
+      "gmane.comp.gcc.devel"
+      "gmane.comp.gdb.devel"
+      "gmane.comp.gnu.binutils"
+      "gmane.comp.lib.glibc.alpha"
+    ];
 
-      groups = pipe source.groups [
-        attrValues
-        concatLists
-      ];
-    }
-    // optionalAttrs (source ? username) {
-      inherit (source)
-        passwordCredential
-        username
-        ;
-    }
-  ) news;
+    olduse = [
+      "net.arch"
+      "net.general"
+      "net.emacs"
+      "net.movies"
+      "net.games.trivia"
+      "net.games.video"
+      "net.lang.c"
+      "net.lang.c++"
+      "net.lang.forth"
+      "net.lang.lisp"
+      "net.music.classical"
+      "net.music.synth"
+      "net.books"
+      "net.sf-lovers"
+      "net.astro"
+      "net.space"
+      "net.crypt"
+      "mod.compilers"
+      "mod.std.c"
+      "net.unix"
+      "net.unix-wizards"
+      "net.usenix"
+    ];
+
+    solani = [
+      "comp.arch"
+      "alt.callahans"
+      "gnu.emacs.gnus"
+      "rec.arts.movies.current-films"
+      "rec.arts.movies.past-films"
+      "rec.games.trivia"
+      "comp.lang.forth"
+      "comp.programming"
+      "comp.os.linux.misc"
+      "rec.music.classical.recordings"
+      "rec.music.misc"
+      "rec.music.rock-pop-r+b.1950s"
+      "rec.arts.sf.written"
+      "sci.astro"
+      "comp.security.unix"
+      "comp.unix.programmer"
+    ];
+  };
 in
 {
   imports = [
@@ -53,9 +110,8 @@ in
         inn = {
           enable = mkDefault true;
 
-          groups = pipe sources [
+          groups = pipe archiveGroups [
             attrValues
-            (map (source: source.groups))
             concatLists
           ];
 
@@ -68,7 +124,11 @@ in
     (mkIf (config.services.inn.role == "primary") {
       services = {
         inn = {
-          upstreams = sources;
+          upstreams = mapAttrs (_: groups: {
+            inherit
+              groups
+              ;
+          }) archiveGroups;
         };
       };
     })
